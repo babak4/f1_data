82fb90fe17dfc7b4925db41f2248bb03a288f9ad
Babak Tourani <babak4@yahoo.com>
2018-06-21 18:40:51 +0100

    0015 - Gathering Schema Statistics at the end of deployment

--- install.sql
+++ install.sql
@@ -1,3 +1,4 @@
+PROMPT Creating External Tables
 @@scripts/CreateETable_Circuits.sql
 @@scripts/CreateETable_Constructors.sql
 @@scripts/CreateETable_ConstructorResults.sql
@@ -25,4 +26,7 @@ PROMPT Creating Sample Schema
 @@scripts/CreateTable_Status.sql
 @@scripts/CreateTable_Results.sql
 
+PROMPT Gathering Schema Statistics
+EXEC DBMS_STATS.GATHER_SCHEMA_STATS(user);
+PROMPT Gathering Schema Statistics - Done!
 exit
